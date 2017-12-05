91c4ff39c016b1f097e0d92ec276934c3fb88030
Kateryna <kateryna.bohatova@gmail.com>
2017-12-05 21:34:46 +0200

    Lesson_18_05122017

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BKN1402
+ * Date: 05.12.2017
+ * Time: 20:03
+ */
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class AboutController extends Controller
+{
+    /**
+     * @Route("/about", name="about_show")
+     *
+     */
+    public function show(SessionInterface $session)
+    {
+        $url = $this->generateUrl('category_show', [
+            'slug'=>'notebooks',
+            'param'=>'getparam'], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return $this->render('about\about.html.twig', ['notebooksUrl' => $url,
+            'lastCategory' => $session->get('lastVisitedCategory')
+        ]);
+    }
+
+    /**
+     * @Route("/to-about")
+     */
+    public function redirectToShow()
+    {
+        return $this->redirectToRoute('about_show');
+    }
+}
